5f1bf8e8d775e06e2557c36428b627a0b77a00ef
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-05-18 19:01:13 +0200

    fix: me query

--- src/gql/User/queries.js
+++ src/gql/User/queries.js
@@ -1,15 +1,15 @@
-const User  = require('../../models/User');
+const User = require('../../models/User');
 
-const me = (_, __, { authorized, user }) => {
+const me = async (_, __, { authorized, user }) => {
 	if (!authorized) throw new Error('Access denied.');
 
-  const dbUser = await User.findById(user.id);
-  if(!dbUser) throw new Error('User not found.');
+	const dbUser = await User.findById(user.id);
+	if (!dbUser) throw new Error('User not found.');
 
-  return {
-    id : dbUser._id,
-    email: dbUser.email,
-  }
+	return {
+		id: dbUser._id,
+		email: dbUser.email,
+	};
 };
 
 module.exports = {
