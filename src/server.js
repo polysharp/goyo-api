3a67b625de635a1f538ea77c5fe755513421598c
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-05-22 18:01:18 +0200

    feat: migrating to apollo-server-express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const { ApolloServer } = require('apollo-server');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
 
 const { typeDefs, User } = require('./gql');
@@ -7,40 +8,44 @@ const db = require('./db');
 
 db();
 
+const app = express();
+
 const resolvers = {
-	Query: {
-		me: User.queries.me,
-	},
-	Mutation: {
-		signUp: User.mutations.signUp,
-		signIn: User.mutations.signIn,
-	},
+  Query: {
+    me: User.queries.me,
+  },
+  Mutation: {
+    signUp: User.mutations.signUp,
+    signIn: User.mutations.signIn,
+  },
 };
 
 const server = new ApolloServer({
-	typeDefs,
-	resolvers,
-	tracing: process.env.NODE_ENV !== 'production',
-	introspection: true,
-	context: ({ req }) => {
-		try {
-			const { authorization } = req.headers;
-			if (authorization) {
-				const parts = authorization.trim().split(' ');
-				if (parts.length === 2 && parts[0] === 'Bearer') {
-					const { id } = jwt.verify(parts[1], process.env.ACCESS_SECRET);
-					return {
-						authorized: true,
-						userId: id,
-					};
-				}
-			}
-		} catch (error) {
-			return { authorized: false };
-		}
-	},
+  typeDefs,
+  resolvers,
+  tracing: process.env.NODE_ENV !== 'production',
+  introspection: true,
+  context: ({ req }) => {
+    try {
+      const { authorization } = req.headers;
+      if (authorization) {
+        const parts = authorization.trim().split(' ');
+        if (parts.length === 2 && parts[0] === 'Bearer') {
+          const { id } = jwt.verify(parts[1], process.env.ACCESS_SECRET);
+          return {
+            authorized: true,
+            userId: id,
+          };
+        }
+      }
+    } catch (error) {
+      return { authorized: false };
+    }
+  },
 });
 
-server.listen(process.env.PORT || 3000).then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+server.applyMiddleware({ app, path: '/', cors: false });
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🚀  Server ready`);
 });
