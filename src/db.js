74efda5961c83dff42791eaa0f5e5033f83730f6
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-24 19:26:11 +0200

    refactor: using config and constants

--- src/db.js
+++ src/db.js
@@ -1,26 +1,28 @@
 const mongoose = require('mongoose');
 
+const { MONGO_DB_URI, MONGO_DB_NAME } = require('./config');
+
 module.exports = async () => {
-	try {
-		await mongoose.connect(
-			process.env.MONGO_DB_URI,
-			{
-				useUnifiedTopology: true,
-				useNewUrlParser: true,
-				useCreateIndex: true,
-				dbName: process.env.MONGO_DB_NAME,
-			},
-			(error) => {
-				if (error) {
-					console.error(error);
-					return error;
-				}
-				console.log(`Connected to ${process.env.MONGO_DB_NAME} db.`);
-				return null;
-			},
-		);
-	} catch (err) {
-		console.log('Failed connection to MONGO DATABASE');
-		console.error(err.message);
-	}
+  try {
+    await mongoose.connect(
+      MONGO_DB_URI,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        dbName: MONGO_DB_NAME,
+      },
+      (error) => {
+        if (error) {
+          console.error(error);
+          return error;
+        }
+        console.log(`Connected to ${MONGO_DB_NAME} db.`);
+        return null;
+      }
+    );
+  } catch (err) {
+    console.log(`Failed connection to ${MONGO_DB_NAME}`);
+    console.error(err.message);
+  }
 };
--- src/server.js
+++ src/server.js
@@ -1,92 +1,24 @@
-require('dotenv').config();
-
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const jwt = require('jsonwebtoken');
+const { CORS_OPTIONS } = require('./constants');
+const { COOKIE_SECRET } = require('./config');
 
-const { typeDefs, User } = require('./gql');
 const db = require('./db');
-
-const cookieConfig = {
-  httpOnly: true,
-  secure: true,
-  signed: true,
-};
+const { authMiddleware } = require('./middlewares');
+const { typeDefs, User } = require('./gql');
 
 db();
 
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://goyo.netlify.app'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
+app.use(cors(CORS_OPTIONS));
+app.use(cookieParser(COOKIE_SECRET));
 
-app.use(cors(corsOptions));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-
-app.use((req, res, next) => {
-  try {
-    const tokenCookie = req.signedCookies['x-cookie-token'];
-    const refreshCookie = req.signedCookies['x-cookie-refresh-token'];
-
-    if (refreshCookie && tokenCookie) {
-      const { id: tokenId } = jwt.verify(tokenCookie, process.env.ACCESS_SECRET);
-      const { id: refreshId } = jwt.verify(refreshCookie, process.env.REFRESH_SECRET);
-
-      const token = jwt.sign(
-        {
-          id: tokenId,
-        },
-        process.env.ACCESS_SECRET,
-        {
-          expiresIn: '1d',
-        }
-      );
-      const refresh = jwt.sign(
-        {
-          id: refreshId,
-        },
-        process.env.REFRESH_SECRET,
-        {
-          expiresIn: '7d',
-        }
-      );
-
-      res.cookie('x-cookie-token', token, { ...cookieConfig, maxAge: 86400000 });
-      res.cookie('x-cookie-refresh-token', refresh, { ...cookieConfig, maxAge: 604800000 });
-
-      req.authorized = true;
-    }
-
-    if (refreshCookie === undefined) {
-      req.authorized = false;
-    }
-
-    if (tokenCookie === undefined && refreshCookie) {
-      req.authorized = true;
-    }
-
-    if (refreshCookie === false || tokenCookie === false) {
-      req.authorized = false;
-    }
-    next();
-  } catch (error) {
-    req.authorized = false;
-    next();
-  }
-});
+app.use(authMiddleware);
 
 const resolvers = {
   Query: {
@@ -104,21 +36,21 @@ const server = new ApolloServer({
   tracing: process.env.NODE_ENV !== 'production',
   introspection: true,
   context: ({ req }) => {
-    try {
-      const { authorization } = req.headers;
-      if (authorization) {
-        const parts = authorization.trim().split(' ');
-        if (parts.length === 2 && parts[0] === 'Bearer') {
-          const { id } = jwt.verify(parts[1], process.env.ACCESS_SECRET);
-          return {
-            authorized: true,
-            userId: id,
-          };
-        }
-      }
-    } catch (error) {
-      return { authorized: false };
-    }
+    // try {
+    //   const { authorization } = req.headers;
+    //   if (authorization) {
+    //     const parts = authorization.trim().split(' ');
+    //     if (parts.length === 2 && parts[0] === 'Bearer') {
+    //       const { id } = jwt.verify(parts[1], process.env.ACCESS_SECRET);
+    //       return {
+    //         authorized: true,
+    //         userId: id,
+    //       };
+    //     }
+    //   }
+    // } catch (error) {
+    //   return { authorized: false };
+    // }
   },
 });
 
