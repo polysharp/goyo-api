c6ec7925d188a27a579916a8d68f38e6a56d9af0
Henri Fardeau <henri.fardeau@outlook.fr>
2020-05-25 23:21:26 +0200

    fix: cookie policy

--- src/constants.js
+++ src/constants.js
@@ -28,17 +28,15 @@ const EXPIRATION_COOKIE_NAME = 'x-expiration-cookie';
 const COOKIE_OPTIONS = Object.freeze({
   auth: {
     httpOnly: true,
-    secure: false,
+    secure: NODE_ENV === 'production',
     signed: true,
     maxAge: NODE_ENV === 'production' ? 24 * 60 * 60 * 1000 : 60 * 1000,
-    domain: NODE_ENV === 'production' ? 'goyo.netlify.app' : 'localhost',
   },
   fake: {
     httpOnly: false,
-    secure: false,
+    secure: NODE_ENV === 'production',
     signed: false,
     maxAge: NODE_ENV === 'production' ? 24 * 60 * 60 * 1000 : 60 * 1000,
-    domain: NODE_ENV === 'production' ? 'goyo.netlify.app' : 'localhost',
   },
 });
 
