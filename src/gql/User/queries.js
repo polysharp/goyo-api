39ed5820f2f3e875eee267a79f582e306cf6e658
Dr-Greg <grigor.mouradyan@outlook.fr>
2020-05-25 20:15:34 +0200

    chore: adding http-status-codes

--- src/gql/User/queries.js
+++ src/gql/User/queries.js
@@ -1,7 +1,8 @@
+const HTTP_CODES = require('http-status-codes');
 const { User } = require('../../models/User');
 
-const me = async (_, __, { authenticated, userId }) => {
-  if (!authenticated) throw new Error('Access denied.');
+const me = async (_, __, { authenticated, userId, res }) => {
+  if (!authenticated) return res.status(HTTP_CODES.UNAUTHORIZED);
 
   const user = await User.findById(userId, 'id email firstName lastName language currency');
   if (!user) throw new Error('User not found.');
